1290947d9529cb9bd047ad9220fe9ab85109c2cc
Hisoda0330 <huangpro01@gmail.com>
2024-10-06 15:04:14 -0400

    finish README.md

---
+++ src/Piece.java
@@ -0,0 +1,19 @@
+// Piece.java
+public class Piece {
+    private char symbol;  // The symbol representing the piece, e.g., 'X' or 'O'
+
+    // Constructor
+    public Piece(char symbol) {
+        this.symbol = symbol;
+    }
+
+    // Getter for symbol
+    public char getSymbol() {
+        return symbol;
+    }
+
+    // Setter for symbol
+    public void setSymbol(char symbol) {
+        this.symbol = symbol;
+    }
+}
